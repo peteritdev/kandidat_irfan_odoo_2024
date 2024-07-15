c4ff302d55f943edd801005964914a1f247c7cc4
irfanmauly <irfanmauli86@gmail.com>
2024-07-15 16:13:31 +0700

    Soal Bagian 2 Repair for methode query on report stock

--- daily_report_inventory/wizard/daily_report_stock.py
+++ daily_report_inventory/wizard/daily_report_stock.py
@@ -8,8 +8,8 @@ class DailyReportInventory(models.TransientModel):
     _name="daily.report.inventory"
     _description='Daily Report Inventory'
 
-    warehouse_id = fields.Many2one('stock.warehouse', string="Plant")
-    product_id = fields.Many2one('product.product', string="SKU")		
+    location_id = fields.Many2one('stock.location', string="Plant", required=True)
+    product_ids = fields.Many2many('product.product', string="Sperpat", required=True)		
     from_date = fields.Date('From Date', default=lambda self: fields.Date.to_string(date.today().replace(day=1)),
                             required=True)
     to_date = fields.Date("To Date", default=lambda self: fields.Date.to_string(
@@ -18,170 +18,84 @@ class DailyReportInventory(models.TransientModel):
     
     def print_report(self):
         datas = []
+        locations = self.env["stock.location"].search([("id", "child_of", [self.location_id.id])])
+        self._cr.execute(
+            """
+            SELECT move.date, move.product_id, move.product_qty,
+                move.product_uom_qty, move.product_uom, move.reference,
+                move.location_id, move.location_dest_id,
+                case when move.location_dest_id in %s
+                    then move.product_qty end as product_in,
+                case when move.location_id in %s
+                    then move.product_qty end as product_out,
+                case when move.date < %s then True else False end as is_initial
+            FROM stock_move move
+            WHERE (move.location_id in %s or move.location_dest_id in %s)
+                and move.state = 'done' and move.product_id in %s
+                and CAST(move.date AS date) < %s
+            ORDER BY move.date, move.reference
+            """,
+            (
+                tuple(locations.ids),
+                tuple(locations.ids),
+                self.from_date,
+                tuple(locations.ids),
+                tuple(locations.ids),
+                tuple(self.product_ids.ids),
+                self.from_date,
+            ),
+            )
+        stock_last_results = self._cr.dictfetchall()
 
-        if self.warehouse_id and not self.product_id:
-            warehouse = self.env['stock.move'].search(
-                        [('location_dest_id.warehouse_id', '=', self.warehouse_id.id), ('state', '=', 'done')]).product_id
-
-            for produk in warehouse:
-                stock_inventory_current = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_dest_id.warehouse_id', '=', self.warehouse_id.id), ('date', '<', self.from_date),
-                             ('location_id.usage', '=', 'inventory'), ('state', '=', 'done')])
-                stock_in_current  = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_dest_id.warehouse_id', '=', self.warehouse_id.id), ('date', '<', self.from_date), 
-                             ('location_dest_id.name', '=', 'Stock'), ('picking_type_id.code', '=', ('incoming', 'mrp_operation')), ('state', '=', 'done')])
-                stock_out_current  = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_id.warehouse_id', '=', self.warehouse_id.id), ('date', '<', self.from_date), 
-                             ('location_dest_id.name', '!=', 'Stock'), ('state', '=', 'done')])
-                
-                current_stock = ((sum(stock_inventory_current.mapped('product_uom_qty')) + sum(stock_in_current.mapped('product_uom_qty'))) - sum(stock_out_current.mapped('product_uom_qty')))
-
-                stock_inventory = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_dest_id.warehouse_id', '=', self.warehouse_id.id), ('date', '>=', self.from_date), 
-                             ('date', '<=', self.to_date), ('location_id.usage', '=', 'inventory'), ('state', '=', 'done')])
-                stock_in = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_dest_id.warehouse_id', '=', self.warehouse_id.id), ('date', '>=', self.from_date), 
-                             ('date', '<=', self.to_date), ('location_dest_id.name', '=', 'Stock'), ('picking_type_id.code', '=', ('incoming', 'mrp_operation')), ('state', '=', 'done')])
-                stock_out = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_id.warehouse_id', '=', self.warehouse_id.id), ('date', '>=', self.from_date), 
-                             ('date', '<=', self.to_date), ('location_dest_id.name', '!=', 'Stock'), ('state', '=', 'done')])
+        if self.product_ids and self.location_id:
+            self._cr.execute(
+            """
+            SELECT move.date, move.product_id, move.product_qty,
+                move.product_uom_qty, move.product_uom, move.reference,
+                move.location_id, move.location_dest_id,
+                case when move.location_dest_id in %s
+                    then move.product_qty end as product_in,
+                case when move.location_id in %s
+                    then move.product_qty end as product_out,
+                case when move.date < %s then True else False end as is_initial
+            FROM stock_move move
+            WHERE (move.location_id in %s or move.location_dest_id in %s)
+                and move.state = 'done' and move.product_id in %s
+                and CAST(move.date AS date) <= %s
+            ORDER BY move.date, move.reference
+            """,
+            (
+                tuple(locations.ids),
+                tuple(locations.ids),
+                self.from_date,
+                tuple(locations.ids),
+                tuple(locations.ids),
+                tuple(self.product_ids.ids),
+                self.to_date,
+            ),
+            )
+            stock_card_results = self._cr.dictfetchall()
+            for rec in self.product_ids:
+                stock = [line for line in stock_card_results if line['product_id'] == rec.id]
+                stock_last = [line for line in stock_last_results if line['product_id'] == rec.id]
+                current_stock = sum(line.get('product_in', 0) or 0 for line in stock_last) - sum(line.get('product_out', 0) or 0 for line in stock_last)
             
                 datas.append({
-                            'id': produk.id,
-                            'code_product': produk.default_code,
-                            'product_id': produk.name,
-                            'product_uom': produk.uom_id.name,
-                            'stock_inventory': current_stock,
-                            'stock_in': sum(stock_in.mapped('product_uom_qty')),
-                            'stock_out': sum(stock_out.mapped('product_uom_qty')),
-                            'stock_remaining': ((current_stock + sum(stock_in.mapped('product_uom_qty'))) - sum(stock_out.mapped('product_uom_qty'))),
+                    'id': rec.id,
+                    'code_product': rec.default_code,
+                    'product_id': rec.name,
+                    'product_uom': rec.uom_id.name,
+                    'stock_inventory': current_stock,
+                    'stock_in': sum(line.get('product_in', 0) or 0 for line in stock),
+                    'stock_out': sum(line.get('product_out', 0) or 0 for line in stock),
+                    'stock_remaining': ((current_stock + sum(line.get('product_in', 0) or 0 for line in stock)) - sum(line.get('product_out', 0) or 0 for line in stock)),
                 })
-        
-        if self.product_id and not self.warehouse_id:
-            warehouse_name = self.env['stock.move'].search([('product_id', '=', self.product_id.id),('state', '=', 'done')]).location_dest_id.warehouse_id
-            for rek in warehouse_name:
-                warehouse_data = {
-                                'name': rek.name,
-                                'warehouse': []
-                                }
-                warehouse = self.env['stock.move'].search([('product_id', '=', self.product_id.id), ('location_dest_id.warehouse_id', '=', rek.id), ('state', '=', 'done')])
-                for produk in warehouse:
-                    stock_inventory_current = self.env['stock.move'].search(
-                                [('product_id', '=', produk.product_id.id), ('location_dest_id.warehouse_id', '=', rek.id), ('date', '<', self.from_date),
-                                 ('location_id.usage', '=', 'inventory'), ('state', '=', 'done')])
-                    stock_in_current = self.env['stock.move'].search(
-                            [('product_id', '=', produk.product_id.id), ('location_dest_id.warehouse_id', '=', rek.id), ('date', '<', self.from_date), 
-                             ('location_dest_id.name', '=', 'Stock'), ('picking_type_id.code', '=', ('incoming', 'mrp_operation')), ('state', '=', 'done')])
-                    stock_out_current = self.env['stock.move'].search(
-                            [('product_id', '=', produk.product_id.id), ('location_id.warehouse_id', '=', rek.id), ('date', '<', self.from_date), 
-                             ('location_dest_id.name', '!=', 'Stock'), ('state', '=', 'done')])
-                    
-                    current_stock = ((sum(stock_inventory_current.mapped('product_uom_qty')) + sum(stock_in_current.mapped('product_uom_qty'))) - sum(stock_out_current.mapped('product_uom_qty')))
-
-                    stock_inventory = self.env['stock.move'].search(
-                                [('product_id', '=', produk.product_id.id), ('location_dest_id.warehouse_id', '=', rek.id), ('date', '>=', self.from_date),
-                                ('date', '<=', self.to_date), ('location_id.usage', '=', 'inventory'), ('state', '=', 'done')])
-                    stock_in = self.env['stock.move'].search(
-                            [('product_id', '=', produk.product_id.id), ('location_dest_id.warehouse_id', '=', rek.id), ('date', '>=', self.from_date), 
-                            ('date', '<=', self.to_date), ('location_dest_id.name', '=', 'Stock'), ('picking_type_id.code', '=', ('incoming', 'mrp_operation')), ('state', '=', 'done')])
-                    stock_out = self.env['stock.move'].search(
-                            [('product_id', '=', produk.product_id.id), ('location_id.warehouse_id', '=', rek.id), ('date', '>=', self.from_date), 
-                            ('date', '<=', self.to_date), ('location_dest_id.name', '!=', 'Stock'), ('state', '=', 'done')])
-            
-                    warehouse_data['warehouse'].append({
-                                'id': produk.product_id.id,
-                                'code_product': produk.product_id.default_code,
-                                'product_id': produk.product_id.name,
-                                'product_uom': produk.product_id.uom_id.name,
-                                'stock_inventory': current_stock,
-                                'stock_in': sum(stock_in.mapped('product_uom_qty')),
-                                'stock_out': sum(stock_out.mapped('product_uom_qty')),
-                                'stock_remaining': ((current_stock + sum(stock_in.mapped('product_uom_qty'))) - sum(stock_out.mapped('product_uom_qty'))),
-                    })
-                datas.append(warehouse_data)
-        
-        if self.product_id and self.warehouse_id:
-            stock_inventory_current = self.env['stock.move'].search(
-                            [('product_id', '=', self.product_id.id), ('location_dest_id.warehouse_id', '=', self.warehouse_id.id), ('date', '<', self.from_date),
-                             ('location_id.usage', '=', 'inventory'), ('state', '=', 'done')])
-            stock_in_current = self.env['stock.move'].search(
-                            [('product_id', '=', self.product_id.id), ('location_dest_id.warehouse_id', '=', self.warehouse_id.id), ('date', '<', self.from_date), 
-                             ('location_dest_id.name', '=', 'Stock'), ('picking_type_id.code', '=', ('incoming', 'mrp_operation')), ('state', '=', 'done')])
-            stock_out_current = self.env['stock.move'].search(
-                            [('product_id', '=', self.product_id.id), ('location_id.warehouse_id', '=', self.warehouse_id.id), ('date', '<', self.from_date), 
-                             ('location_dest_id.name', '!=', 'Stock'), ('state', '=', 'done')])
-
-            current_stock = ((sum(stock_inventory_current.mapped('product_uom_qty')) + sum(stock_in_current.mapped('product_uom_qty'))) - sum(stock_out_current.mapped('product_uom_qty')))
-
-            stock_inventory = self.env['stock.move'].search(
-                            [('product_id', '=', self.product_id.id), ('location_dest_id.warehouse_id', '=', self.warehouse_id.id), ('date', '>=', self.from_date),
-                             ('date', '<=', self.to_date), ('location_id.usage', '=', 'inventory'), ('state', '=', 'done')])
-            stock_in = self.env['stock.move'].search(
-                            [('product_id', '=', self.product_id.id), ('location_dest_id.warehouse_id', '=', self.warehouse_id.id), ('date', '>=', self.from_date), 
-                             ('date', '<=', self.to_date), ('location_dest_id.name', '=', 'Stock'), ('picking_type_id.code', '=', ('incoming', 'mrp_operation')), ('state', '=', 'done')])
-            stock_out = self.env['stock.move'].search(
-                            [('product_id', '=', self.product_id.id), ('location_id.warehouse_id', '=', self.warehouse_id.id), ('date', '>=', self.from_date), 
-                             ('date', '<=', self.to_date), ('location_dest_id.name', '!=', 'Stock'), ('state', '=', 'done')])
-            
-            datas.append({
-                          'id': self.product_id.id,
-                          'code_product': self.product_id.default_code,
-                          'product_id': self.product_id.name,
-                          'product_uom': self.product_id.uom_id.name,
-                          'stock_inventory': current_stock,
-                          'stock_in': sum(stock_in.mapped('product_uom_qty')),
-                          'stock_out': sum(stock_out.mapped('product_uom_qty')),
-                          'stock_remaining': ((current_stock + sum(stock_in.mapped('product_uom_qty'))) - sum(stock_out.mapped('product_uom_qty'))),
-                })
-        
-
-        if not self.warehouse_id and not self.product_id:
-            warehouse_name = self.env['stock.move'].search([('state', '=', 'done')]).location_dest_id.warehouse_id
-            for rek in warehouse_name:
-                warehouse_data = {
-                                'name': rek.name,
-                                'warehouse': []
-                                }
-                warehouse = self.env['stock.move'].search([('location_dest_id.warehouse_id', '=', rek.id), ('state', '=', 'done')]).product_id 
-                for produk in warehouse:
-                    stock_inventory_current = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_dest_id.warehouse_id', '=', rek.id), ('date', '<', self.from_date), 
-                             ('location_id.usage', '=', 'inventory'), ('state', '=', 'done')])
-                    stock_in_current = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_dest_id.warehouse_id', '=', rek.id), ('date', '<', self.from_date), 
-                             ('location_dest_id.name', '=', 'Stock'), ('picking_type_id.code', '=', ('incoming', 'mrp_operation')), ('state', '=', 'done')])
-                    stock_out_current = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_id.warehouse_id', '=', rek.id), ('date', '<', self.from_date), 
-                             ('location_dest_id.name', '!=', 'Stock'), ('state', '=', 'done')])
-                    
-                    current_stock = ((sum(stock_inventory_current.mapped('product_uom_qty')) + sum(stock_in_current.mapped('product_uom_qty'))) - sum(stock_out_current.mapped('product_uom_qty')))
-                    
-                    stock_inventory = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_dest_id.warehouse_id', '=', rek.id), ('date', '>=', self.from_date), ('date', '<=', self.to_date), 
-                             ('location_id.usage', '=', 'inventory'), ('state', '=', 'done')])
-                    stock_in = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_dest_id.warehouse_id', '=', rek.id), ('date', '>=', self.from_date), ('date', '<=', self.to_date), 
-                             ('location_dest_id.name', '=', 'Stock'), ('picking_type_id.code', '=', ('incoming', 'mrp_operation')), ('state', '=', 'done')])
-                    stock_out = self.env['stock.move'].search(
-                            [('product_id', '=', produk.id), ('location_id.warehouse_id', '=', rek.id), ('date', '>=', self.from_date), ('date', '<=', self.to_date), 
-                             ('location_dest_id.name', '!=', 'Stock'), ('state', '=', 'done')]) 
-                    warehouse_data['warehouse'].append({
-                                'id': produk.id,
-                                'code_product': produk.default_code,
-                                'product_id': produk.name,
-                                'product_uom': produk.uom_id.name,
-                                'stock_inventory': current_stock,
-                                'stock_in': sum(stock_in.mapped('product_uom_qty')),
-                                'stock_out': sum(stock_out.mapped('product_uom_qty')),
-                                'stock_remaining': ((sum(stock_inventory.mapped('product_uom_qty'))+sum(stock_in.mapped('product_uom_qty')))-sum(stock_out.mapped('product_uom_qty'))),
-                    })
-                datas.append(warehouse_data)
+    
 
 
         res = {
             'stock':datas,
-            'warehouse': self.warehouse_id.name,
-            'product': self.product_id.name,
+            'location': self.location_id.name,
             'start_date': str(self.from_date.strftime('%d-%m-%Y')),
             'end_date': str(self.to_date.strftime('%d-%m-%Y')),
             'date': str(self.from_date.strftime('%B %Y')),
